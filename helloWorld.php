51d81641ff73bab20e3aebe1cc9a0e26fbe2f0cc
Oskar Nikopensius <nikopeos@greeny.cs.tlu.ee>
2016-09-14 08:54:21 +0300

    fist lines of code

---
+++ helloWorld.php
@@ -0,0 +1,33 @@
+<?php
+
+	
+
+	$firstName = "Oskar";
+	$lastName = "Nikopensius";
+	echo $firstName." ".$lastName;
+	
+	//echo $firstName;
+	//echo $lastName;
+
+	$number1 = 5;
+	$number2 = 6;
+
+	//echo $number2 * $number1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
